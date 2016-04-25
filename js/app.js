ca686a902402364644b7997e850d91bd33197389
nhan712 <chinhan712@gmail.com>
2016-04-25 10:40:05 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
-var HelloMessage = React.createClass({
+var Button = React.createClass({
+  clickEvent: function() {
+	  console.log(this.props.btnNm);
+  },
   render: function() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+	  <button type="button" className="btn btn-primary" onClick={this.clickEvent}>{this.props.btnNm}</button>
+    );
   }
 });
 
-ReactDOM.render(<HelloMessage name="John" />, mountNode);
+ReactDOM.render(<Button btnNm="Get Image" />, document.getElementById('btn1'));
+ReactDOM.render(<Button btnNm="Get Link" />, document.getElementById('btn2'));

7ef42c3c8da7713a4bbaf7388c6cff4878dadf7c
nhan712 <chinhan712@gmail.com>
2016-05-17 17:13:15 +0700

    update

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ var Button = React.createClass({
   },
   render: function() {
     return (
-	  <button type="button" className="btn btn-primary" onClick={this.clickEvent}>{this.props.btnNm}</button>
+	  <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onClick={this.clickEvent}>{this.props.btnNm}</button>
     );
   }
 });
 
-ReactDOM.render(<Button btnNm="Get Image" />, document.getElementById('btn1'));
-ReactDOM.render(<Button btnNm="Get Link" />, document.getElementById('btn2'));
+ReactDOM.render(<Button btnNm="Get Images" />, document.getElementById('btn1'));
+ReactDOM.render(<Button btnNm="Get Links" />, document.getElementById('btn2'));
